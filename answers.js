799fd3c925fe4771d22b1943633a8d8c80ee81d8
SageofWar <87577824+SageofWar@users.noreply.github.com>
2021-07-30 18:08:44 -0400

    Add files via upload

---
+++ answers.js
@@ -0,0 +1,14 @@
+//Part II: JavaScript Reps
+
+//Write a for loop that will log the numbers 1 through 20.
+for(counter = 0; counter <= 20; counter++){
+    console.log(counter)
+}
+//"Easy Going answered"
+
+//Write a for loop that will log only the even numbers in 0 through 200.
+for(counter = 0; counter <= 200; counter++){
+    if(counter % 2 == 0){
+        console.log(counter)
+    }
+}
